259245811d8333c1c453511f90a98e3dff4d821e
skwldwld <yezi0512@handong.ac.kr>
2026-07-03 22:29:34 +0900

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  base: '/Basketball/',
+  base: "/Basketball/",
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        game: resolve(__dirname, 'game.html')
-      }
-    }
-  }
-})
+        main: resolve(__dirname, "index.html"),
+        game: resolve(__dirname, "game.html"),
+        result: resolve(__dirname, "result.html"),
+      },
+    },
+  },
+});
